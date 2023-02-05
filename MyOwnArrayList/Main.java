4645e8747b70c26b2ba76bd8f39e6f5a96ddb330
Saba Koguashvili <110823186+jpyesk@users.noreply.github.com>
2023-02-06 03:40:02 +0400

    Update Main.java

--- MyOwnArrayList/Main.java
+++ MyOwnArrayList/Main.java
@@ -1,8 +1,20 @@
 package MyOwnArrayList;
 
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        MyArraylist<Integer> lst = new MyArraylist<>();
+       lst.add(22);
+       lst.add(23);
+       lst.add(24);
+       lst.add(25);
+       lst.add(26);
+       lst.add(27);
+
+        for (int i = 0; i < lst.size(); i++) {
+            System.out.println(lst.get(i));
+
+        }
+        
     }
 }
